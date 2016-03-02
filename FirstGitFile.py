900899c6f0f364a268929ac1eebc5a812f7d9afb
Ben S <ben.s.s@hotmail.co.uk>
2016-03-02 17:17:06 +0000

    I was correct I had solved the issue that I was concerned about. The new version has a loop to work out the chances of having a shared birthday. Although actually it is flawed since we only care about it being successful at least once per group. Bear with me

--- FirstGitFile.py
+++ FirstGitFile.py
@@ -1,14 +1,19 @@
 from random import randint
-numberofstudents = int(input("How many students are in the class?\n>> "))
-listofstudents = [randint(1,365) for z in range(numberofstudents)]
-occurrences = 0
-countI = 0
-for i in listofstudents[:len(listofstudents)-1]:
-    countI += 1
-    countP = 1
-    for p in listofstudents[countI:]:
-        countP += 1
-        if i == p:
-            print "{}, person {}'s birthday is equal to {} person {}'s birthday".format(i,countI,p,countP)
-            occurrences += 1
-print "The total number of occurrences is {} over the course of {} students.".format(occurrences, numberofstudents)
+numberofstudents = int(input("How many students are in the class\n >> "))
+numberofruns = int(input("How many times do you want to run the test?\n >> "))
+total_occurences = 0
+for s in range(numberofruns):
+    listofstudents = [randint(1,365) for z in range(numberofstudents)]
+    occurrences = 0
+    countI = 0
+    for i in listofstudents[:len(listofstudents)-1]:
+        countI += 1
+        countP = 1
+        for p in listofstudents[countI:]:
+            countP += 1
+            if i == p:
+                occurrences += 1
+    total_occurences += occurrences
+chances_of_having_same_birthday = (float(total_occurences)/float(numberofruns))*100
+print "The chance of sharing a birthday over the course of {} students, is {}%.".format(numberofstudents,
+                                                                                       chances_of_having_same_birthday)
