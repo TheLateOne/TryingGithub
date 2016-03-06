a84d931a4c8ad50037c2eab48ca7102ace4485e1
Ben S <ben.s.s@hotmail.co.uk>
2016-03-06 20:41:23 +0000

    Created the structure of the file and added triangle area calculator. Added subsequent formulas to complete the tool

--- ShapeAreaFinder.py
+++ ShapeAreaFinder.py
@@ -1,20 +1,25 @@
+import math
+
 def find_shape():
     shape = raw_input("What shape do you want to find the area of [square/triangle/circle/rectangle]? \n >> ")
     return eval(shape)
 
 def square():
-    print "IT WORKED"
+    edge_length = raw_input("What are the lengths of the two sides of the quadrilateral? \n >> ")
+    x = edge_length.split()
+    return int(x[0]) * int(x[1])
 
 def triangle():
     lengths_tri = raw_input("What is the height and base of the triangle? \n >> ")
     x = lengths_tri.split()
-    z = (float(x[0]) * float(x[1])) / 2
-    return z
+    return (float(x[0]) * float(x[1])) / 2
 
 def circle():
-    pass
+    circle_info = input("What is the radius of the circle? \n >> ")
+    return math.pi * circle_info ** 2
+
 def rectangle():
-    square()
+    return square()
 
 def run():
     s = find_shape()
