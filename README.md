5269cfadba2179469c7965e89f451d3984b940ff
Ben S <ben.s.s@hotmail.co.uk>
2016-03-28 21:38:05 +0100

    Changed the data aggregator to a function so that it can be easily imported and run with any stocks you wish to use.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TryingGithub
+
+This is a collection of programs, hosted on GitHub for my own personal benefit. There are some mathematically based
+programs, a golf score card type program and a program which grabs stock prices at a defined period in order to create
+an inter-minute data set. I will also host the data sets here for public usage and over time I will probably expand
+to a fairly large subsection of the FTSE 100 or some personalised subsection.
--- first_stocks_program.py
+++ first_stocks_program.py
@@ -2,33 +2,55 @@ from yahoo_finance import Share
 import time
 import datetime
 
-Ticker = "CHRT.L"
-x = Share(Ticker)
+Stock_Ticker = "CHRT.L"
+x = Share(Stock_Ticker)
 start = datetime.time(8, 00, 00)
 end = datetime.time(16, 30, 00)
+file = "Cohort_PLC.txt"
 
-count = 0
-# with open("Cohort_PLC.txt", "a") as prices:
-while True:
-    prices = open("Cohort_PLC.txt", "a")
-    # concerning this "open" "close" within the loop; this means that regardless of how the program exits we still
-    # write to the file (as opposed to what we were doing which was saving only temporarily
-    t1 = datetime.datetime.now().time()
-    if not start <= t1 <= end:
-        print "We're going to sleep it off"
-        # This finds the current time and waits until the market should next open (does not account for weekends)
-        p = datetime.datetime.today()
-        y = p.replace(hour=start.hour, minute=start.minute, second=start.second)
-        delta_t = y - p
-        if "-1" in str(delta_t):
-            # in case of the "start" time being before the current time we shift to the next day
-            y = p.replace(day=p.day + 1, hour=start.hour, minute=start.minute, second=start.second)
+
+def get_market_times(Market_Name):
+    # Cleaner to make this a function - add any relevant market time zones here (In UTC). Check wikipedia for list
+    if Market_Name.lower() == "lse":
+        start2 = datetime.time(8, 00, 00)
+        end2 = datetime.time(16, 30, 00)
+    elif Market_Name.lower() == "nyse":
+        start2 = datetime.time(14, 30, 00)
+        end2 = datetime.time(21, 00, 00)
+    elif Market_Name.lower() == "tse":
+        start2 = datetime.time(00, 00, 00)
+        end2 = datetime.time(6, 00, 00)
+    elif Market_Name.lower() == "krx":
+        start2 = datetime.time(00, 00, 00)
+        end2 = datetime.time(6, 00, 00)
+    else:
+        return "Market_Error: Market unsupported, enter times in code"
+    return start2, end2
+
+
+def inter_minute_data_get(Ticker, Market, Target_File):
+    start, end = get_market_times(Market)
+    while True:
+        prices = open(Target_File, "a")
+        # concerning this "open" "close" within the loop; this means that regardless of how the program exits we still
+        # write to the file (as opposed to what we were doing which was saving only temporarily
+        t1 = datetime.datetime.utcnow().time()
+        if not start <= t1 <= end:
+            # This finds the current time and waits until the market should next open (does not account for weekends)
+            p = datetime.datetime.utcnow()
+            y = p.replace(hour=start.hour, minute=start.minute, second=start.second)
             delta_t = y - p
-        h, m, s = str(delta_t).split(":")
-        new_delta = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s)).total_seconds()
-        print new_delta
-        time.sleep(new_delta)
-    prices.write("Time: {}Ticker: {} Price: {} \n".format(Share.get_trade_datetime(x).strip("UTC+0000"), Ticker,
-                                                          Share.get_price(x)))
-    time.sleep(3)
-    prices.close()
+            if "-1" in str(delta_t):
+                # in case of the "start" time being before the current time we shift to the next day
+                y = p.replace(day=p.day + 1, hour=start.hour, minute=start.minute, second=start.second)
+                delta_t = y - p
+            h, m, s = str(delta_t).split(":")
+            new_delta = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s)).total_seconds()
+            time.sleep(new_delta)
+        prices.write("Time: {}Ticker: {} Price: {} \n".format(Share.get_trade_datetime(x).strip("UTC+0000"), Ticker,
+                                                              Share.get_price(x)))
+        time.sleep(3)
+        prices.close()
+
+
+inter_minute_data_get(Stock_Ticker, "nyse", file)
