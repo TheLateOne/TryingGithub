f334266ccd0bd11abe5edf7f7cde46680958c35b
Ben S <ben.s.s@hotmail.co.uk>
2016-04-09 09:22:39 +0100

    Initial commit of 'Text Adventure' with basic info and starting structure

---
+++ Text Adventure V1.py
@@ -0,0 +1,15 @@
+# Adventure Game V1 [Untitled]
+# Aim of the Game - Reach the Mountain Top!
+# Some events will rely on luck to overcome but your inventory will affect your "luckiness"
+# GLHF
+
+def Welcome_Player():
+#	global name
+	name = raw_input("What is your character's name? \n >> ")
+	print "Well {} your adventure awaits, you have to climb this mountain but beware, it will not be easy!".format(name)
+
+def Lowest_Level(name):
+	pass
+
+Welcome_Player
+print "hello"
